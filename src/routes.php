e2d5d90428262af6c94617626c100ba3b5fe6ba0
Kevin Hill <kevinkhill@gmail.com>
2013-11-06 08:47:45 -0800

    removed link to icons file

--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,5 @@
 <?php
 
 Route::get('fontawesome', function() {
-    $icons = file(__DIR__.'/../fontawesome-icons.txt', FILE_IGNORE_NEW_LINES);
-
-    return View::make('fontawesome::home')
-        ->with('icons', $icons);
+    return View::make('fontawesome::home');
 });
